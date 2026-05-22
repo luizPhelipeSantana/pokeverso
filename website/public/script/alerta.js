94810726b5acb6c650f1aa726bb9e2cf8976c87f
luizPhelipeSantana <luiz.oliveira@sptech.school>
2026-05-22 10:41:22 -0300

    Finalizado a tela do quiz

--- website/public/script/alerta.js
+++ website/public/script/alerta.js
@@ -2,9 +2,12 @@ function exibirAlerta(texto) {
     alerta_texto.innerHTML = `
         <p>${texto}</p>
     `;
+    alerta.style.zIndex = "20000";
     alerta.style.opacity = "1";
 
+
     setTimeout(() => {
+        alerta.style.zIndex = "0";
         alerta.style.opacity = "0";
     }, 3000);
 }
